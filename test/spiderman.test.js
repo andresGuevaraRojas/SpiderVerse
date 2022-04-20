aade28fab2cb35ce7f6e412044af96b3a20499ce
Andres Guevara <=>
2022-04-19 23:39:11 -0500

    Se Creo el test para el metodo getInfo() de la clase Spiderman

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,11 +1,17 @@
+const Spiderman = require('../app/Spiderman')
+
 describe("Unit Test for Spiderman Class",()=>{
     test("Tes 1) Create a spiderman object",()=>{
-        const andrewGarfield = new spiderman("Spiderman Sony",31,"Andrew Garfield",2,"Sony")
+        const andrewGarfield = new Spiderman("Spiderman Sony",31,"Andrew Garfield",2,"Sony")
 
         expect(andrewGarfield.name).toBe("Spiderman Sony")
         expect(andrewGarfield.age).toBe(31)
         expect(andrewGarfield.actor).toBe("Andrew Garfield")
-        expect(andrewGarfield.movies).toBe(31)
+        expect(andrewGarfield.movies).toBe(2)
         expect(andrewGarfield.studio).toBe("Sony")
+    })
+    test("Test 2) Use the method getInfo()",()=>{
+        const tomHolland = new Spiderman("Spiderman Marvel",25,"Tom Holland",4,"Marvel")
+        expect(tomHolland.getInfo()).toBe("Hey, I'm Tom Holland from Marvel Studio")
     })
 })
